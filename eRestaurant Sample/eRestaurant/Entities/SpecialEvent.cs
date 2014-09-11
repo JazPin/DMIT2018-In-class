6eb4bdaee0fa8d5e6d93d5eabfe90d9c2cb5ee88
JazPin <jpin1@studentmail.nait.ca>
2014-09-11 09:28:41 -0600

    Add region

--- eRestaurant Sample/eRestaurant/Entities/SpecialEvent.cs
+++ eRestaurant Sample/eRestaurant/Entities/SpecialEvent.cs
@@ -9,9 +9,11 @@ namespace eRestaurant.Entities
 {
     public class SpecialEvent
     {
+        #region Public Prpoerties
         [Key]
         public string EventCode { get; set; }
         public string Description { get; set; }
         public bool Active { get; set; }
+        #endregion
     }
 }
