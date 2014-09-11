0a731893677ab489f4f89271392b1a774f32f314
JazPin <jpin1@studentmail.nait.ca>
2014-09-11 09:18:26 -0600

    Create class for E-Restaurant "Table" table

---
+++ eRestaurant Sample/eRestaurant/Entities/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    public class Table
+    {
+        // By Convention, if we use a property with the same name as the class, 
+        // but with a suffix of ID, then Entity Framework will recongnize that property 
+        // as mapping to the database table's Primary Key column.
+        public int TableID { get; set; }
+        public byte TableNumber { get; set; }
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+
+    }
+}
