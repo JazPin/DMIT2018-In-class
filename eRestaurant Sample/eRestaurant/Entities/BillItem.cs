5977206e08ec226d6885f2accd59123e5028b133
JazPin <jpin1@studentmail.nait.ca>
2014-09-16 14:20:38 -0600

    Add Attribute to  BillItem table

--- eRestaurant Sample/eRestaurant/Entities/BillItem.cs
+++ eRestaurant Sample/eRestaurant/Entities/BillItem.cs
@@ -16,8 +16,14 @@ namespace eRestaurant.Entities
         [Column(Order = 2)]
         public int ItemID { get; set; }
 
+        [Required(ErrorMessage="Quntity is required")]
+        [Range(1,20, ErrorMessage="Quantity msut be between 1 and 20")]  
         public int Quantity { get; set; }
+        [Required(ErrorMessage="Sale Price is required")]
+        [Range(0.00,50,ErrorMessage="Sale Price must be between zero and $50,00 inclusive")]
         public decimal Saleprice { get; set; }
+        [Required(ErrorMessage = "Unit Cost is required")]
+        [Range(0.01, 30, ErrorMessage = "Unit Cost must be between $0.01 and $30.00 inclusive")]
         public decimal UnitCost { get; set; }
         public string Notes { get; set; }
 
