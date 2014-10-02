18f475b2db6fb11d4a59af657602811d7a975d92
JazPin <jpin1@studentmail.nait.ca>
2014-10-02 09:26:33 -0600

    Modify MenuItems List

--- eRestaurant Sample/eRestaurant/BLL/MenuItemController.cs
+++ eRestaurant Sample/eRestaurant/BLL/MenuItemController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data.Entity;
 using eRestaurant.DLL;
 using eRestaurant.Entities;
+using eRestaurant.Entities.DTOs;
 
 namespace eRestaurant.BLL
 {
@@ -22,6 +23,32 @@ namespace eRestaurant.BLL
                 // Get the Item data and include the Category data for each item.
                 // The .Include() method on the Dbset<T> class performs "eager loading" of data.
                 return context.Items.Include(it => it.MenuCategory).ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Category> ListCategoryMenuItem()
+        {
+            using(var context = new RestaurantContext())
+            {
+                var data = from cat in context.MenuCategories
+                           orderby cat.Description
+                           select new Category() // Anonymous type
+                           {
+                               Description = cat.Description,
+                               MenuItems =    from item in cat.MenuItems
+                                              where item.Active
+                                              orderby item.Description
+                                              select new MenuItem()
+                                              {
+                                                  Description = item.Description,
+                                                  Price = item.CurrentPrice,
+                                                  Calories = item.Calories,
+                                                  Comment = item.Comment
+                                              }
+                           };
+
+                return data.ToList();
             }
         }
     }
