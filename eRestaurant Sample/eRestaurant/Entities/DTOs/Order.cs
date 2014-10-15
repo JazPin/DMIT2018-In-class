3ab7904440e12cf63d46848344a66441dce2c119
JazPin <jpin1@studentmail.nait.ca>
2014-10-15 13:16:44 -0600

    Add Order class

--- eRestaurant Sample/eRestaurant/Entities/DTOs/Order.cs
+++ eRestaurant Sample/eRestaurant/Entities/DTOs/Order.cs
@@ -15,6 +15,42 @@ namespace eRestaurant.Entities.DTOs
         public bool Served { get; set; }
         public string OrderComments { get; set; }
 
-        public List<OrderItem> Items { get; set; }
+        public decimal TotalAmount
+        {
+            get
+            {
+                decimal value = 0; // default value
+                if(Items != null)
+                {
+                    value = Items.Sum(x => x.ItemTotal); // See footnote #1
+                }
+                return value; 
+            }
+        }
+
+        public IList<OrderItem> Items { get; set; }
     }
 }
+
+
+/* FOOTNOTES
+ * #1 A lamda expression is just an inline anonymous function call.
+
+    the following lamda expression:
+ *          Items.Sum(x => x.ItemTotal)
+ *          
+ *  is the same as this inline delegate:
+ *          Items.Sum( delegate(OrderItem x)
+ *                      {
+ *                          reuturn x.ItemTotal;
+ *                      }
+ *                   )
+ *  and the same as:
+ *      //GetItemTotal is the name of a methods; it must be cast because it cannot be automaticlly inferred
+ *      Items.Sum((Func<OrdrItem, decimal>)GetItemTotal)
+ *      ...
+ *      private static decimal GetItemTotal(OrderItem x)
+ *      {
+ *          return x.ItemTotal;
+ *      }
+*/
