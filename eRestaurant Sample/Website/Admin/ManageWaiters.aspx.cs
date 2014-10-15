722db5ae9c7fd604d7541a465fca885ce54a3570
JazPin <jpin1@studentmail.nait.ca>
2014-10-15 14:57:52 -0600

    Add user control to the web pages

--- eRestaurant Sample/Website/Admin/ManageWaiters.aspx.cs
+++ eRestaurant Sample/Website/Admin/ManageWaiters.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eRestaurant.BLL;
+using eRestaurant.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,60 @@ public partial class Admin_ManageWaiters : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            // TODO : List he waiters in the dropdown list
+        }
+    }
+    protected void Add_Click(object sender, EventArgs e)
+    {
+        MessageUserControl.TryRun(AddWaiter, "Added waiter", "The new Waiter was successfully added");
+    }
+    public void AddWaiter()
+    {
+        Waiter person = new Waiter()
+        {
+            FirstName = FirstName.Text,
+            LastName = LastName.Text,
+            Address = Address.Text,
+            Phone = Phone.Text,
+            HireDate = DateTime.Parse(HireDate.Text),
+        };
+        DateTime fireOn;
+        if (DateTime.TryParse(ReleaseDate.Text, out fireOn))
+            person.ReleaseDate = fireOn;
+        var controller = new RestaurantAdminController();
+        person.WaiterID = controller.AddWaiter(person);
+        WaiterID.Text = person.WaiterID.ToString();
+        //TODO: Re-populate dropdown list of waiters and set the selectedvalue as well
+    }
+    protected void Update_Click(object sender, EventArgs e)
+    {
+        int temp;
+        if (int.TryParse(WaiterID.Text, out temp))
+        {
+            MessageUserControl.TryRun(UpdateWaiter, "Update Successed", "The waiter information was updated.");
+        }
+        else
+            MessageUserControl.ShowInfo("Please lookup a waiter before clicking 'Update'.");
+    }
+    public void UpdateWaiter()
+    {
+        Waiter person = new Waiter()
+        {
+            WaiterID = int.Parse(WaiterID.Text),
+            FirstName = FirstName.Text,
+            LastName = LastName.Text,
+            Address = Address.Text,
+            Phone = Phone.Text,
+            HireDate = DateTime.Parse(HireDate.Text),
+        };
+        DateTime fireOn;
+        if (DateTime.TryParse(ReleaseDate.Text, out fireOn))
+            person.ReleaseDate = fireOn;
+        var controller = new RestaurantAdminController();
+        controller.UpdateWaiter(person);
+        //WaiterID.Text = person.WaiterID.ToString();
+        //TODO: Re-populate dropdown list of waiters and set the selectedvalue as well
     }
 }
